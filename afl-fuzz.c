2e3fe8e543bc05143f150a305fdd1666e175e1cd
Anastasios Andronidis <anastasios.andronidis@gmail.com>
2022-05-24 14:20:14 +0100

    Reduce short runs to 1.000 runs

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -3608,7 +3608,7 @@ static u8 run_target(char** argv, u32 timeout) {
   total_execs++;
 #ifdef LONG_BENCH
 #elif SHORT_BENCH
-  if (total_execs == 10000) {
+  if (total_execs == 1000) {
     OKF("Snapfuzz-bench: Done!");
     raise(SIGINT);
   }
